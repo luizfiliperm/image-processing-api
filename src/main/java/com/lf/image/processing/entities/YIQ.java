5e8ef58e27e4207968edc9abd8ce1b1bc51f203e
luizfiliperm <luizfilipemr17@gmail.com>
2024-04-01 18:04:07 -0300

    Implementing YiqService

---
+++ src/main/java/com/lf/image/processing/entities/YIQ.java
@@ -0,0 +1,33 @@
+package com.lf.image.processing.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.awt.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class YIQ {
+
+    private Double y;
+
+    private Double i;
+
+    private Double q;
+
+
+    public YIQ(Color color){
+        this.y = 0.299 * color.getRed() + 0.587 * color.getGreen() + 0.114 * color.getBlue();
+        this.i = 0.596 * color.getRed() - 0.274 * color.getGreen() - 0.322 * color.getBlue();
+        this.q = 0.211 * color.getRed() - 0.523 * color.getGreen() + 0.312 * color.getBlue();
+    }
+
+    public int convertToRGB(){
+        int red = (int) (this.y + 0.956 * this.i + 0.621 * this.q);
+        int green = (int) (this.y - 0.272 * this.i - 0.647 * this.q);
+        int blue = (int) (this.y - 1.106 * this.i + 1.703 * this.q);
+        return new Color(red, green, blue).getRGB();
+    }
+}
---
+++ src/main/java/com/lf/image/processing/services/impl/YiqServiceImpl.java
@@ -0,0 +1,70 @@
+package com.lf.image.processing.services.impl;
+
+import com.lf.image.processing.entities.Image;
+import com.lf.image.processing.entities.YIQ;
+import com.lf.image.processing.services.YiqService;
+import org.springframework.stereotype.Service;
+
+import java.awt.*;
+
+@Service
+public class YiqServiceImpl implements YiqService {
+
+    @Override
+    public void increaseY(Image image, int value) {
+
+        for(int i = 0; i < image.getWidth(); i++){
+            for(int j = 0; j < image.getHeight(); i++){
+                YIQ yiq = new YIQ(new Color(image.getBufferedImage().getRGB(i,j)));
+                YIQ newYiq = new YIQ(getYValue(yiq.getY(), value), yiq.getI(), yiq.getQ());
+                image.getBufferedImage().setRGB(i,j, newYiq.convertToRGB());
+            }
+        }
+    }
+
+    @Override
+    public void increaseI(Image image, int value) {
+        for(int i = 0; i < image.getWidth(); i++){
+            for(int j = 0; j < image.getHeight(); i++){
+                YIQ yiq = new YIQ(new Color(image.getBufferedImage().getRGB(i,j)));
+                YIQ newYiq = new YIQ(yiq.getY(), getIValue(yiq.getI(), value), yiq.getQ());
+                image.getBufferedImage().setRGB(i,j, newYiq.convertToRGB());
+            }
+        }
+    }
+
+    @Override
+    public void increaseQ(Image image, int value) {
+        for(int i = 0; i < image.getWidth(); i++){
+            for(int j = 0; j < image.getHeight(); i++){
+                YIQ yiq = new YIQ(new Color(image.getBufferedImage().getRGB(i,j)));
+                YIQ newYiq = new YIQ(yiq.getY(), yiq.getI(), getQValue(yiq.getQ(), value));
+                image.getBufferedImage().setRGB(i,j, newYiq.convertToRGB());
+            }
+        }
+    }
+
+    private Double getYValue(Double y, int value){
+        if(y + value < 0)
+            return 0.0;
+        if(y + value > 1)
+            return 1.0;
+        return y + value;
+    }
+
+    private Double getIValue(Double i, int value){
+        if(i + value < -0.5957)
+            return -0.5957;
+        if(i + value > 0.5957)
+            return 0.5957;
+        return i + value;
+    }
+
+    private Double getQValue(Double q, int value){
+        if(q + value < -0.5226)
+            return -0.5226;
+        if(q + value > 0.5226)
+            return 0.5226;
+        return q + value;
+    }
+}
